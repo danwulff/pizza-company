5d680d7ff40146f0b7d976b45b47e0ffdd6f7ed8
Daniel Wulff <danielwulff.ee@gmail.com>
2016-04-08 12:31:14 -0700

    created html and some styling, added images

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//business logic
+
+//user interface logic
+$(document).ready(function(){
+var pizzas = [];
+
+  //anytime the pizza amount is changed
+  $("#pizzaAmount").change(function(){
+    //clear previous pizzas
+    pizzas = [];
+
+    //create pizza objects
+
+    //update list
+
+    //show main content
+  });
+});
